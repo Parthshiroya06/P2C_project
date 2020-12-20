1fa5c3d46b3f9498738656fb497dea35d321bf2f
NISHANT SANANDIYA <nishantsanandiya22@gmail.com>
2020-12-20 17:32:11 +0530

    done with login and registion , now move to navigation activity

---
+++ app/src/main/java/com/example/p2c_project/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.p2c_project
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun logout(view: View) {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, Login_Activity::class.java))
+        finish()
+    }
+}
